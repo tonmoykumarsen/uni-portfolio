645ad917be465039fa8286213a7b561da60acaa9
tonmoykumarsen <jessijesica217@gmail.com>
2025-02-19 15:54:02 +0600

    CompleteAll

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-function hamburg() {
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
-}
 
-function cancel() {
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
+        // Typewriter Effect
+        const texts = ["Developer", "Designer", "Youtuber"];
+        let textIndex = 0;
+        let charIndex = 0;
+        const textElement = document.querySelector(".typewriter-text");
+        
+        function type() {
+            if (charIndex < texts[textIndex].length) {
+                textElement.textContent += texts[textIndex].charAt(charIndex);
+                charIndex++;
+                setTimeout(type, 100);
+            } else {
+                setTimeout(erase, 2000);
+            }
+        }
+        
+        function erase() {
+            if (charIndex > 0) {
+                textElement.textContent = texts[textIndex].substring(0, charIndex - 1);
+                charIndex--;
+                setTimeout(erase, 50);
+            } else {
+                textIndex = (textIndex + 1) % texts.length;
+                setTimeout(type, 500);
+            }
+        }
+        
+        document.addEventListener("DOMContentLoaded", () => {
+            setTimeout(type, 500);
+        });
 
-// Typewriter Effect
-const texts = [
-    "DEVELOPER",
-    "DESIGNER",
-    "YOUTUBER"
-]
-let speed = 100;
-const textElements = document.querySelector(".typewriter-text");
-let textIndex = 0;
-let charcterIndex = 0;
-
-function typeWriter() {
-    if (charcterIndex < texts[textIndex].length) {
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed);
-    }
-    else {
-        setTimeout(eraseText, 1000)
-    }
-}
-
-function eraseText() {
-    if (textElements.innerHTML.length > 0) {
-        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
-        setTimeout(eraseText, 50)
-    }
-    else {
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500)
-    }
-}
-
-window.onload = typeWriter;
+        // Smooth scrolling for navigation links
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
